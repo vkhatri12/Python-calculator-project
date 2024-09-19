3870561de6600bdf98e77b0d6ecc2ea46c206ae2
Vivan <manyadc@yahoo.com>
2024-09-18 23:57:46 -0400

    Initial command/setup

---
+++ New Python Calculator.py
@@ -0,0 +1,22 @@
+# Python Calculator
+# Assign variables
+operator = input("Enter an operator (+ - * /): ")
+num1 = float(input("Enter the 1st number: "))
+num2 = float(input("Enter the 2nd number: "))
+# Use if statements
+if operator == "+":
+    result = num1 + num2
+    print(round(result, 3))
+elif operator == "-":
+    result = num1 - num2
+    print(round(result, 3))
+elif operator =="*":
+    result = num1 * num2
+    print(round(result, 3))
+elif operator =="/":
+    result = num1 / num2
+    print(round(result, 3))
+else:
+    print(f"{operator} is invalid")
+
+
